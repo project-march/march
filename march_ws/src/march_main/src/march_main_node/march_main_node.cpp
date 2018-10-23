dd9fb1e02ca6e877159d65614dfc9ea8d7c662e2
tim <timbuckers@gmail.com>
2018-10-23 17:12:45 +0200

    Simple test object TestCube Style accordingly to google style guide

--- march_ws/src/march_main/src/march_main_node/march_main_node.cpp
+++ march_ws/src/march_main/src/march_main_node/march_main_node.cpp
@@ -5,22 +5,22 @@
 #include "march_main_node.h"
 
 int main(int argc, char **argv) {
-    std::cout << "test";
-    ros::init(argc, argv, "adele");
-    ros::NodeHandle n;
 
-    ros::Publisher hello_pub = n.advertise<std_msgs::String>("talking", 1000);
+  std::cout << "test";
+  ros::init(argc, argv, "adele");
+  ros::NodeHandle n;
 
-    ros::Rate loop_rate(10);
-    while (ros::ok()) {
-        std_msgs::String msg;
-        msg.data = "hello";
-        ROS_INFO("%s", msg.data.c_str());
-        hello_pub.publish(msg);
-        ros::spinOnce();
-        loop_rate.sleep();
-    }
+  ros::Publisher hello_pub = n.advertise<std_msgs::String>("talking", 1000);
 
+  ros::Rate loop_rate(10);
+  while (ros::ok()) {
+    std_msgs::String msg;
+    msg.data = "hello";
+    ROS_INFO("%s", msg.data.c_str());
+    hello_pub.publish(msg);
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
 
-    return 0;
+  return 0;
 }
