feb0ceedfea2495073a1d9a457fa4bc07f8a2e61
tim <timbuckers@gmail.com>
2018-10-23 14:58:21 +0200

    CMakeLists.txt build and add main node

---
+++ march_ws/src/march_main/src/march_main_node.cpp
@@ -0,0 +1,5 @@
+//
+// Created by tim on 23-10-18.
+//
+
+#include "march_main_node.h"
---
+++ march_ws/src/march_main/src/march_main_node.h
@@ -0,0 +1,14 @@
+//
+// Created by tim on 23-10-18.
+//
+
+#ifndef MARCH_MAIN_MARCH_MAIN_NODE_H
+#define MARCH_MAIN_MARCH_MAIN_NODE_H
+
+
+class march_main_node {
+
+};
+
+
+#endif //MARCH_MAIN_MARCH_MAIN_NODE_H
