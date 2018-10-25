697f89539103a7f7ba8e06ec1073ccdd0e638816
tim <timbuckers@gmail.com>
2018-10-25 20:57:32 +0200

    Service working

--- march_ws/src/march_main/src/data_logger.cpp
+++ march_ws/src/march_main/src/data_logger.cpp
@@ -9,5 +9,6 @@
 int main(int argc, char **argv) {
   ros::init(argc, argv, "gait_controller_node");
   ros::NodeHandle n;
+  ros::spin();
   return 0;
 }
--- march_ws/src/march_main/src/master_node.cpp
+++ march_ws/src/march_main/src/master_node.cpp
@@ -9,18 +9,17 @@
 #include <march_custom_msgs/Gait.h>
 #include <march_custom_msgs/GaitInstruction.h>
 
-ros::ServiceClient client;
 
 void developerInputCallBack(const march_custom_msgs::Gait::ConstPtr &msg) {
   ROS_INFO("I heard: [gait: %ld]", msg->gait);
   march_custom_msgs::GaitInstruction srv;
-  srv.request.gait = msg->gait;
-  if (client.call(srv)) {
-    const char *output = srv.response.result.c_str();
-    ROS_INFO("MasterNode %s", output);
-  } else {
-    ROS_ERROR("Failed to call service gait_instructions");
-  }
+//    srv.request.gait = msg->gait;
+//  if (client.call(srv)) {
+//    const char *output = srv.response.result.c_str();
+//    ROS_INFO("MasterNode %s", output);
+//  } else {
+//    ROS_ERROR("Failed to call service gait_instructions");
+//  }
 }
 
 int main(int argc, char **argv) {
@@ -28,6 +27,14 @@ int main(int argc, char **argv) {
   ros::NodeHandle n;
   ros::Subscriber sub = n.subscribe("developer_input", 1000, developerInputCallBack);
   ros::ServiceClient client = n.serviceClient<march_custom_msgs::GaitInstruction>("gait_instructions");
+  march_custom_msgs::GaitInstruction srv;
+  srv.request.gait = 1;
+  if (client.call(srv)) {
+    const char *output = srv.response.result.c_str();
+    ROS_INFO("MasterNode %s", output);
+  } else {
+    ROS_ERROR("Failed to call service gait_instructions");
+  }
   ros::spin();
   return 0;
 }
