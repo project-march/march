36e457dcc5d0e3e1fe9c26d5d1821a6e50dbd218
tim <timbuckers@gmail.com>
2018-10-29 11:40:08 +0100

    catkin lint failing on warning level W1

--- march_ws/build.sh
+++ march_ws/build.sh
@@ -6,7 +6,7 @@ catkin_make || exit 1
 catkin_make roslint_march_main || exit 1
 
 # Catkin lint, fail on errors only. TODO remove missing_directory.
-catkin_lint src/*/ --ignore missing_directory --ignore literal_project_name --explain -W0 || exit 1
+catkin_lint src/*/ --ignore missing_directory --ignore literal_project_name --ignore missing_install_target --explain -W1 || exit 1
 
 # Run the tests, ensuring the path is set correctly.
 source devel/setup.bash || exit 1
