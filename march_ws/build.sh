58c723ac56896074306c346c2e38b0ead4931fe3
Isha <isha@projectmarch.nl>
2018-11-07 11:25:40 +0100

    Add custom failure message to each step of the build process

--- march_ws/build.sh
+++ march_ws/build.sh
@@ -1,22 +1,38 @@
 #!/usr/bin/env bash
 
-catkin_make || exit 1
+build_passed () {
+    notify "Your local build has passed!"
+
+}
+
+
+build_failed () {
+    notify "$1"
+    exit 1
+}
+
+notify () {
+    if [ -x "$(command -v notify-send)" ]; then
+        notify-send -i $PWD/walking.png "$1"
+    fi
+}
+
+catkin_make || build_failed "Catkin_make failed"
 
 # cpplinter.
-catkin_make roslint_march_main || exit 1
+catkin_make roslint_march_main || build_failed "ros_lint failed"
 
 # Catkin lint, fail on errors only. TODO remove missing_directory.
-catkin_lint src/*/ --ignore missing_directory --ignore literal_project_name --ignore missing_install_target --explain -W1 || exit 1
+catkin_lint src/*/ --ignore missing_directory --ignore literal_project_name --ignore missing_install_target --explain -W1 || build_failed "catkin_lint failed"
 
 # Run the tests, ensuring the path is set correctly.
 source devel/setup.bash || exit 1
-catkin_make run_tests && catkin_test_results || exit 1
+catkin_make run_tests && catkin_test_results || build_failed "Tests failed"
 
 for directory in src/*/launch
 do
-    rosrun roslaunch roslaunch-check $directory || exit 1
+    rosrun roslaunch roslaunch-check $directory || build_failed "roslaunch-check failed in directory $directory"
 done
 
-if [ -x "$(command -v notify-send)" ]; then
-    notify-send -i $PWD/walking.png "Your local build has passed!"
-fi
+build_passed
+
