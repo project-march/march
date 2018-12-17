327aa37cf2594a240ab90d4e69b5b2088b6276c2
Isha Dijcks <ishadijcks@gmail.com>
2018-12-17 12:21:29 +0100

    Rename custom msgs

---
+++ march_ws/src/march_shared_resources/include/march_shared_resources/TopicNames.h
@@ -0,0 +1,29 @@
+// Copyright 2018 Project March.
+#ifndef MARCH_MAIN_TOPICNAMES_H
+#define MARCH_MAIN_TOPICNAMES_H
+
+#include <string>
+namespace TopicNames
+{
+const char* const right_ankle_position = "march/right_ankle_position_controller/command";
+const char* const gait_status = "gait/status";
+const char* const left_hip_position = "march/left_hip_position_controller/command";
+const char* const left_knee_position = "march/left_knee_position_controller/command";
+const char* const left_ankle_position = "march/left_ankle_position_controller/command";
+const char* const right_hip_position = "march/right_hip_position_controller/command";
+const char* const right_knee_position = "march/right_knee_position_controller/command";
+
+const char* const perform_gait = "march/perform_gait";
+
+};  // namespace TopicNames
+
+namespace ServiceNames
+{
+const char* const urdf_validation = "march/urdf_validation";
+const char* const config_validation = "march/config_validation";
+const char* const xml_validation = "march/xml_validation";
+
+const char* const request_gait_file = "march/request_gait_file";
+};
+
+#endif  // MARCH_MAIN_TOPICNAMES_H
---
+++ march_ws/src/march_shared_resources/include/march_shared_resources/gait_enum.h
@@ -0,0 +1,13 @@
+// Copyright 2018 Project March.
+
+#ifndef SRC_PUBLIC_ENUM_GAIT_ENUM_H
+#define SRC_PUBLIC_ENUM_GAIT_ENUM_H
+
+enum GaitType
+{
+  Walk,
+  Sit,
+  Stand
+};
+
+#endif  // SRC_PUBLIC_ENUM_GAIT_ENUM_H
