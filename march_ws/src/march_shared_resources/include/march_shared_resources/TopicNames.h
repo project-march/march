beb8ad683f3b189bf0c54d3c4570ef2c4bbe6a2b
tim <timbuckers@gmail.com>
2019-01-23 14:28:06 +0100

    old topic names needed for backwards compatibility

--- march_ws/src/march_shared_resources/include/march_shared_resources/TopicNames.h
+++ march_ws/src/march_shared_resources/include/march_shared_resources/TopicNames.h
@@ -46,6 +46,15 @@ const char* const gait_denied = "march/gait/denied";
 
 // Trajectory instruction for ROS Control
 const char* const follow_joint_trajectory_execution = "/march/trajectory_controller/follow_joint_trajectory/goal";
+
+// TODO(Tim): remove the old controller command topics
+const char* const right_ankle_position = "march/right_ankle_position_controller/command";
+const char* const left_hip_position = "march/left_hip_position_controller/command";
+const char* const left_knee_position = "march/left_knee_position_controller/command";
+const char* const left_ankle_position = "march/left_ankle_position_controller/command";
+const char* const right_hip_position = "march/right_hip_position_controller/command";
+const char* const right_knee_position = "march/right_knee_position_controller/command";
+
 };  // namespace TopicNames
 
 namespace ServiceNames
