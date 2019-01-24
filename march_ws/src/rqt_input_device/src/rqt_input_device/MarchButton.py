625d9beac5608aba15992e68cab3a1963ca33f57
Isha Dijcks <ishadijcks@gmail.com>
2019-01-24 16:29:21 +0100

    Fix bug where buttons were required to have an image

--- march_ws/src/rqt_input_device/src/rqt_input_device/MarchButton.py
+++ march_ws/src/rqt_input_device/src/rqt_input_device/MarchButton.py
@@ -7,7 +7,7 @@ class MarchButton:
     """Default size of a button, is used if no other size is given."""
     default_size = QSize(150, 150)
 
-    def __init__(self, name, text=None, image=None, callback=None, size=None):
+    def __init__(self, name, text="", image="", callback=None, size=None):
         self.name = name
         self.text = text
         self.image = image
