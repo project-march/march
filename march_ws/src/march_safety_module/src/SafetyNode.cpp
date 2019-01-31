921f35e1792f1e58a7a0dd41dc9e10c79e2f6378
tim <timbuckers@gmail.com>
2019-01-31 11:48:25 +0100

    add comments and refactor

--- march_ws/src/march_safety_module/src/SafetyNode.cpp
+++ march_ws/src/march_safety_module/src/SafetyNode.cpp
@@ -12,6 +12,11 @@
 ros::Publisher error_publisher;
 double temperature_threshold;
 
+/**
+ * This callback checks if the temperature values do not exceed the defined threshold
+ * @param msg the temperature message
+ * @param sensor_name the name of the sender
+ */
 void temperatureCallback(const sensor_msgs::TemperatureConstPtr& msg, const std::string& sensor_name)
 {
   if (msg->temperature > temperature_threshold)
@@ -32,28 +37,35 @@ void temperatureCallback(const sensor_msgs::TemperatureConstPtr& msg, const std:
   }
 }
 
+void createTemperatureSubscribers(std::vector<ros::Subscriber>* temperature_subscribers,
+                                  std::vector<std::string>* sensor_names, ros::NodeHandle* n)
+{
+  for (std::string sensor_name : *sensor_names)
+  {
+    // use boost::bind to pass on the sensor_name as extra parameter to the callback method
+    ros::Subscriber subscriber_temperature =
+        n->subscribe<sensor_msgs::Temperature>(std::string(TopicNames::temperature) + "/" + sensor_name, 1000,
+                                               boost::bind(temperatureCallback, _1, sensor_name));
+    temperature_subscribers->push_back(subscriber_temperature);
+  }
+}
+
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "march_safety_node");
   ros::NodeHandle n;
   ros::Rate rate(200);
 
+  // Load all parameters and threshold from the launch file
   std::vector<std::string> sensor_names;
   n.getParam("/sensors", sensor_names);
   n.getParam(ros::this_node::getName() + std::string("/temperature_threshold"), temperature_threshold);
 
   // Create a subscriber for each sensor
   std::vector<ros::Subscriber> temperature_subscribers;
+  createTemperatureSubscribers(&temperature_subscribers, &sensor_names, &n);
 
-  for (std::string sensor_name : sensor_names)
-  {
-    // use boost::bind to pass on the sensor_name as extra parameter to the callback method
-    ros::Subscriber subscriber_temperature =
-        n.subscribe<sensor_msgs::Temperature>(std::string(TopicNames::temperature) + "/" + sensor_name, 1000,
-                                              boost::bind(temperatureCallback, _1, sensor_name));
-    temperature_subscribers.push_back(subscriber_temperature);
-  }
-
+  // Create a error publisher to notify the system (state machine) if something is wrong
   error_publisher = n.advertise<march_shared_resources::Error>(TopicNames::error, 1000);
 
   while (ros::ok())
