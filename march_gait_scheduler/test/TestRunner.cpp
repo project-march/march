c3e56526e066b5399757b95bd20061809d9b96a4
Tim Buckers <timbuckers@gmail.com>
2019-05-16 11:18:39 +0200

    add testrunner again, removed by accident

---
+++ march_gait_scheduler/test/TestRunner.cpp
@@ -0,0 +1,20 @@
+// Copyright 2018 Project March.
+
+#include "gtest/gtest.h"
+#include "ros/ros.h"
+
+
+class SchedulerTest : public ::testing::Test {
+ protected:
+};
+
+/**
+ * The main method which runs all the tests
+ */
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "testnode");
+  testing::InitGoogleTest(&argc, argv);
+  auto res = RUN_ALL_TESTS();
+  ros::shutdown();
+  return res;
+}
