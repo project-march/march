f7dc4aeed5f41ee935f896d60c655ce0a93c4a04
Isha Dijcks <ishadijcks@gmail.com>
2019-07-24 10:29:37 +0200

    Seperate tests to avoid safety tests interfering with each other (#294)

--- march_safety/test/TestConnectionLost.cpp
+++ march_safety/test/TestConnectionLost.cpp
@@ -9,57 +9,12 @@
 #include <march_safety/TemperatureSafety.h>
 #include "ErrorCounter.cpp"
 
-class TestConnectionLostError : public ::testing::Test
+class TestConnectionLost : public ::testing::Test
 {
 protected:
 };
 
-TEST_F(TestConnectionLostError, connectionNeverStarted)
-{
-  ros::Time::init();
-  ros::NodeHandle nh;
-  ErrorCounter errorCounter;
-  ros::Subscriber sub = nh.subscribe("march/error", 0, &ErrorCounter::cb, &errorCounter);
-
-  while (0 == sub.getNumPublishers())
-  {
-    ros::Duration(0.1).sleep();
-  }
-  EXPECT_EQ(1, sub.getNumPublishers());
-
-  ros::spinOnce();
-  ros::Duration(0.5).sleep();
-  ros::spinOnce();
-  EXPECT_EQ(0, errorCounter.count);
-}
-
-TEST_F(TestConnectionLostError, connectionNotLost)
-{
-  ros::Time::init();
-  ros::NodeHandle nh;
-  int send_errors_interval;
-  nh.getParam("/march_safety_node/send_errors_interval", send_errors_interval);
-  ros::Publisher pub_alive = nh.advertise<std_msgs::Time>("march/input_device/alive", 0);
-  ErrorCounter errorCounter;
-  ros::Subscriber sub = nh.subscribe("march/error", 0, &ErrorCounter::cb, &errorCounter);
-
-  while (0 == pub_alive.getNumSubscribers() || 0 == sub.getNumPublishers())
-  {
-    ros::Duration(0.1).sleep();
-  }
-  EXPECT_EQ(1, pub_alive.getNumSubscribers());
-  EXPECT_EQ(1, sub.getNumPublishers());
-
-  std_msgs::Time timeMessage;
-  timeMessage.data = ros::Time::now();
-  pub_alive.publish(timeMessage);
-  ros::spinOnce();
-  ros::Duration(send_errors_interval / 2 / 1000).sleep();
-  ros::spinOnce();
-  EXPECT_EQ(0, errorCounter.count);
-}
-
-TEST_F(TestConnectionLostError, connectionLost)
+TEST_F(TestConnectionLost, connectionLost)
 {
   ros::Time::init();
   ros::NodeHandle nh;
