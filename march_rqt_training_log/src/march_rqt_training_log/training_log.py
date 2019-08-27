aaea001700eaf6784990570fe35e655f40929ce6
Olav de Haas <o.n.dehaas@student.tudelft.nl>
2019-08-27 17:25:20 +0200

    clean up

--- march_rqt_training_log/src/march_rqt_training_log/training_log.py
+++ march_rqt_training_log/src/march_rqt_training_log/training_log.py
@@ -1,6 +1,8 @@
 import os
+
 import rospy
 import rospkg
+from std_msgs.msg import String
 
 from qt_gui.plugin import Plugin
 from python_qt_binding import loadUi
@@ -10,47 +12,32 @@ class TrainingLogPlugin(Plugin):
 
     def __init__(self, context):
         super(TrainingLogPlugin, self).__init__(context)
-        # Give QObjects reasonable names
         self.setObjectName('TrainingLogPlugin')
 
-        # Create QWidget
         self._widget = QWidget()
-        # Get path to UI file which should be in the "resource" folder of this package
+        self.init_ui(context)
+
+        self.training_log_publisher = rospy.Publisher('/march/training/log', String, queue_size=10)
+
+    def init_ui(self, context):
         ui_file = os.path.join(rospkg.RosPack().get_path('march_rqt_training_log'), 'resource', 'training_log.ui')
-        # Extend the widget with all attributes and children from UI file
         loadUi(ui_file, self._widget)
-        # Give QObjects reasonable names
+
         self._widget.setObjectName('TrainingLogUi')
-        # Show _widget.windowTitle on left-top of each plugin (when 
-        # it's set in _widget). This is useful when you open multiple 
-        # plugins at once. Also if you open multiple instances of your 
-        # plugin at once, these lines add number to make it easy to 
-        # tell from pane to pane.
+
         if context.serial_number() > 1:
             self._widget.setWindowTitle(self._widget.windowTitle() + (' (%d)' % context.serial_number()))
-        # Add widget to the user interface
+
         context.add_widget(self._widget)
+
         self._widget.LogButton.clicked.connect(self.log)
 
     def shutdown_plugin(self):
-        # TODO unregister all publishers here
-        pass
-
-    def save_settings(self, plugin_settings, instance_settings):
-        # TODO save intrinsic configuration, usually using:
-        # instance_settings.set_value(k, v)
-        pass
-
-    def restore_settings(self, plugin_settings, instance_settings):
-        # TODO restore intrinsic configuration, usually using:
-        # v = instance_settings.value(k)
-        pass
+        rospy.signal_shutdown("rqt plugin closed")
 
     def log(self):
-        print(self._widget.LogText.toPlainText())
+        message = self._widget.LogText.toPlainText()
+        rospy.loginfo(message)
+        self.training_log_publisher.publish(message)
         self._widget.LogText.clear()
 
-    #def trigger_configuration(self):
-        # Comment in to signal that the plugin has a way to configure
-        # This will enable a setting button (gear icon) in each dock widget title bar
-        # Usually used to open a modal configuration dialog
