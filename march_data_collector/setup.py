026334843b303f74434078666b8931facb9301bf
Rutger van Beek <24rutger@gmail.com>
2019-10-28 10:45:11 +0100

    Added launch file and renamed file

--- march_data_collector/setup.py
+++ march_data_collector/setup.py
@@ -7,7 +7,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 setup_args = generate_distutils_setup(
     packages=['march_data_collector'],
     package_dir={'': 'src'},
-    scripts=['scripts/march_data_collector.py'],
+    scripts=['scripts/march_data_collector'],
 )
 
 setup(**setup_args)
