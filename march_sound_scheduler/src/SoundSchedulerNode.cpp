a4a0863f2fbaf1571dc31952e837a4679640e685
Olav de Haas <o.n.dehaas@student.tudelft.nl>
2019-10-31 10:14:49 +0100

    Fix order of sound node topics

--- march_sound_scheduler/src/SoundSchedulerNode.cpp
+++ march_sound_scheduler/src/SoundSchedulerNode.cpp
@@ -12,16 +12,17 @@ int main(int argc, char** argv)
   ros::init(argc, argv, "march_sound_scheduler");
   ros::NodeHandle n;
 
-  ros::Publisher soundPub = n.advertise<sound_play::SoundRequest>("/robotsound", 0);
+  ros::Publisher sound_pub = n.advertise<sound_play::SoundRequest>("/robotsound", 0);
 
-  Scheduler scheduler;
-
-  ros::Subscriber soundSub = n.subscribe("/march/sound/schedule", 10, &Scheduler::scheduleMsg, &scheduler);
-  while (0 == soundPub.getNumSubscribers())
+  while (0 == sound_sub.getNumSubscribers())
   {
     ROS_DEBUG("Waiting on sound play topic");
     ros::Duration(0.1).sleep();
   }
+
+  Scheduler scheduler;
+
+  ros::Subscriber sound_sub = n.subscribe("/march/sound/schedule", 10, &Scheduler::scheduleMsg, &scheduler);
 
   ros::Rate rate(10);
   while (ros::ok())
