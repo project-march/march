ec8caec77331234a43f27d970529a739a863dd41
rogier <rogierkrijnen@live.nl>
2019-11-06 12:10:17 +0100

    Adjust look_up_table method to pull the gains from a parameter server

--- march_gain_scheduling/src/march_gain_scheduling/dynamic_pid_reconfigurer.py
+++ march_gain_scheduling/src/march_gain_scheduling/dynamic_pid_reconfigurer.py
@@ -29,25 +29,9 @@ class DynamicPIDReconfigurer:
                                                    "d": d_value})
             rospy.logdebug("Config set to {0}, {1}, {2}".format(p_value, i_value, d_value))
 
-    # Method that functions as a lookup table. The implementation now only functions as a test and should be adjusted
-    # for working with a config file which stores all the tuned values. Having a lookup table within your node is very
-    # untidy.
+    # Method that pulls the PID values from the gaittype_gains.yaml config file
     def look_up_table(self, i):
-        if self._gait_name == "sit":
-            return 10001, 0, 10
-        elif self._gait_name == "walk":
-            return 10002, 0, 10
-        elif self._gait_name == "stand":
-            return 10003, 0, 10
-        elif self._joint_list[i] == "right_ankle" and self._gait_name == "sit_down":
-            return 10004, 0, 10
-        elif self._joint_list[i] == "left_hip_fe" and self._gait_name == "sit_down":
-            return 10005, 0, 10
-        elif self._joint_list[i] == "right_hip_fe" and self._gait_name == "sit_down":
-            return 10006, 0, 10
-        elif self._joint_list[i] == "left_knee" and self._gait_name == "sit_down":
-            return 10007, 0, 10
-        elif self._joint_list[i] == "right_knee" and self._gait_name == "sit_down":
-            return 10008, 0, 10
-        else:
-            return 1000, 0, 10
+        p_value = rospy.get_param("/gaittypes/"+self._gait_name+"_gains/"+self._joint_list[i]+"/p")
+        i_value = rospy.get_param("/gaittypes/"+self._gait_name+"_gains/"+self._joint_list[i]+"/i")
+        d_value = rospy.get_param("/gaittypes/"+self._gait_name+"_gains/"+self._joint_list[i]+"/d")
+        return p_value, i_value, d_value
