d7f6e7bad5e7ccd718bf0417e6400ec58f34f846
Olav de Haas <o.n.dehaas@student.tudelft.nl>
2019-11-28 16:52:59 +0100

    Remove doc.sh

--- doc.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-packages="march_description march_gait_scheduler march_sound_scheduler march_gait_selection march_launch march_safety march_state_machine"
-
-mkdir -p docs/html
-for package in $packages; do
-    catkin document $package --no-deps
-    mv docs/$package/html docs/html/$package
-done
