3033b6db43bffbbf85c46a48c7b2afa6a8326ff7
Ciliaclaij <54306954+Ciliaclaij@users.noreply.github.com>
2020-01-08 13:56:46 +0100

    Feature/con 61 implementation interpolation (#418)
    * Make interpolation method for gain scheduling.
    
    * Add multiple tests for linear interpolation and file name changes
    
    * Fix typos and enhance current interpolation method
    
    * Fix rosunit test
    
    * Fixed typos and tests to make it work
    
    * First steps for implementation of interpolation; definition of variables needed
    
    * Fixed naming, tests and added exception
    
    * Fixed naming of test and PKG error
    
    * Fixed spacing
    
    * Fixed the stupid quotation mark
    
    * First steps for implementation of interpolation; definition of variables needed
    
    * Add interpolation implementation
    
    * Change stairs gait_type to stairs_like instead of default and fix client_update
    
    * Fix tests by adding second variable
    
    * Fix tests again
    
    * Fixed flake8 errors and changed loginfo to logdebug
    
    * Fixed flake8 errors
    
    * fix joint list get to use rosparam insead of hardcoded list
    
    * Fix potential issue of not linearizing after the first gait has been called. Fix compatibility with the test joint. Add feature of usig the test joint by calling the march.launch with the robot:=test_joint_rotational argument
    
    * Fixed private variables and typo
    
    * Fixed Rogiers beunfix that didn't work
    
    * Added rate.sleep() and separated interpolation_done from one_step_linear_interpolation.py
    
    * Added depend march_gain_scheduling to launch package.xml
    
    * Only stop when joints are done
    
    * Refactored client_update and added walk_like as default (otherwise it did not work for homing)
    
    * Fixed == to is and info to debug
    
    * Remove use of gain scheduling in march.launch, Refactor march_type to configuration in gain scheduling launch file, Allow use of test joint/march robot/simulation specifically in gain scheduling launch file
    
    * remove unecessary launch types in march gain scheduling and add march_rqt_input_device launch and gait_scheduler_node
    
    Co-authored-by: Roelemans <47626418+Roelemans@users.noreply.github.com>
    Co-authored-by: Olav de Haas <olavh@protonmail.com>
    Co-authored-by: Deakensbubbles <36988064+Deakensbubbles@users.noreply.github.com>

--- march_gain_scheduling/src/march_gain_scheduling/dynamic_pid_reconfigurer.py
+++ march_gain_scheduling/src/march_gain_scheduling/dynamic_pid_reconfigurer.py
@@ -1,39 +1,69 @@
 from dynamic_reconfigure.client import Client
 import rospy
 
-from march_shared_resources.msg import GaitNameActionGoal
+from march_shared_resources.msg import GaitActionGoal
+
+from .one_step_linear_interpolation import interpolate
 
 
 class DynamicPIDReconfigurer:
-    def __init__(self, gait_name=None, joint_list=None):
-        self._gait_name = gait_name
+    def __init__(self, gait_type='walk_like', joint_list=None, max_time_step=0.1):
+        self._gait_type = gait_type
         self._joint_list = joint_list
+        self._max_time_step = max_time_step
+        self.current_gains = [self.look_up_table(i) for i in range(len(self._joint_list))]
+        self.interpolation_done = True
+        self.last_update_time = 0
         self._clients = []
         for i in range(len(self._joint_list)):
             self._clients.append(Client('/march/controller/trajectory/gains/' + self._joint_list[i], timeout=30))
-        rospy.Subscriber('/march/gait/schedule/goal', GaitNameActionGoal, callback=self.gait_selection_callback)
+        rospy.Subscriber('/march/gait/schedule/goal', GaitActionGoal, callback=self.gait_selection_callback)
+        self._linearize = rospy.get_param('~linearize_gain_scheduling')
 
     def gait_selection_callback(self, data):
         rospy.logdebug('This is the gait name: %s', data.goal.current_subgait.gait_type)
-        if self._gait_name != data.goal.name:
+        if self._gait_type != data.goal.current_subgait.gait_type or not self.interpolation_done:
             rospy.logdebug('The selected gait: {0} is not the same as the previous gait: {1}'.format(
-                data.goal.current_subgait.gait_type, self._gait_name))
-            self._gait_name = data.goal.current_subgait.gait_type
-            self.client_update()
+                data.goal.current_subgait.gait_type, self._gait_type))
+            self._gait_type = data.goal.current_subgait.gait_type
+            if self._gait_type is None or self._gait_type == '':
+                self._gait_type = 'walk_like'
+                rospy.logwarn('The gait has no gait type, default is set to walk_like')
+            self.interpolation_done = False
+            rate = rospy.Rate(10)
+            while not self.interpolation_done:
+                rate.sleep()
+                self.client_update()
 
     def client_update(self):
+        rospy.logdebug('self.linearize is: {0}'.format(self._linearize))
+        self.interpolation_done = True
+
         for i in range(len(self._joint_list)):
-            p_value, i_value, d_value = self.look_up_table(i)
-            if p_value is not None:
-                self._clients[i].update_configuration({'p': p_value,
-                                                       'i': i_value,
-                                                       'd': d_value})
-                rospy.logdebug('Config set to {0}, {1}, {2}'.format(p_value, i_value, d_value))
+            if self._linearize:
+                needed_gains = self.look_up_table(i)
+                gradient = rospy.get_param('~linear_slope')
+                current_time = rospy.get_time()
+                time_interval = current_time - self.last_update_time
+
+                self.current_gains[i] = interpolate(self.current_gains[i], needed_gains, gradient, time_interval)
+                if self.current_gains[i] != needed_gains:
+                    self.interpolation_done = False
+                self.last_update_time = current_time
+            else:
+                self.current_gains[i] = self.look_up_table(i)
+
+            self._clients[i].update_configuration({'p': self.current_gains[i][0],
+                                                   'i': self.current_gains[i][1],
+                                                   'd': self.current_gains[i][2]})
+            rospy.logdebug('Config set to {0}, {1}, {2}'.format(self.current_gains[i][0],
+                                                                self.current_gains[i][1],
+                                                                self.current_gains[i][2]))
 
     # Method that pulls the PID values from the gains_per_gait_type.yaml config file
-    def look_up_table(self, i):
-        if rospy.has_param('~gait_types/' + self._gait_name + '_gains/'):
-            gains = rospy.get_param('~gait_types/' + self._gait_name + '_gains/' + self._joint_list[i])
-            return gains['p'], gains['i'], gains['d']
+    def look_up_table(self, joint_index):
+        if rospy.has_param('~gait_types/' + self._gait_type):
+            gains = rospy.get_param('~gait_types/' + self._gait_type + '/' + self._joint_list[joint_index])
+            return [gains['p'], gains['i'], gains['d']]
         else:
-            return None, None, None
+            return [None, None, None]
--- march_gain_scheduling/src/march_gain_scheduling/gain_scheduling_node.py
+++ march_gain_scheduling/src/march_gain_scheduling/gain_scheduling_node.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 import rospy
 
 from .dynamic_pid_reconfigurer import DynamicPIDReconfigurer
@@ -6,10 +5,8 @@ from .dynamic_pid_reconfigurer import DynamicPIDReconfigurer
 
 def main():
     # The parameters Joint_list and gait_list should be obtained from the parameter server of the workspace.
-    joint_list = ['left_hip_aa', 'right_hip_aa', 'left_ankle', 'right_ankle', 'left_hip_fe', 'right_hip_fe',
-                  'left_knee', 'right_knee']
-
-    DynamicPIDReconfigurer('random', joint_list)
-
     rospy.init_node('march_gain_scheduling_node')
+    joint_list = rospy.get_param('/march/joint_names')
+    DynamicPIDReconfigurer(joint_list=joint_list)
+
     rospy.spin()
