5aa40a2442ae98b4e12c686775ddcbcd22c3f5d2
Roelemans <47626418+Roelemans@users.noreply.github.com>
2020-03-03 12:11:12 +0100

    Load current gains from parameter sever every gain scheduling (#478)
    * Load current gains from parameter sever every time the gain scheduler callback is called.
    
    * Use normal for loop
    
    * define parameter in init
    
    Co-authored-by: JorisWeeda <45165354+JorisWeeda@users.noreply.github.com>

--- march_gain_scheduling/src/march_gain_scheduling/dynamic_pid_reconfigurer.py
+++ march_gain_scheduling/src/march_gain_scheduling/dynamic_pid_reconfigurer.py
@@ -11,27 +11,27 @@ class DynamicPIDReconfigurer:
         self._gait_type = None
         self._joint_list = joint_list
         self._max_time_step = max_time_step
-        self.current_gains = []
-        for joint_index in range(len(self._joint_list)):
-            gains = rospy.get_param('/march/controller/trajectory/gains/' + self._joint_list[joint_index])
-            self.current_gains.append([gains['p'], gains['i'], gains['d']])
         self.interpolation_done = True
         self.last_update_time = None
         self._clients = []
+        self.current_gains = []
         for i in range(len(self._joint_list)):
             self._clients.append(Client('/march/controller/trajectory/gains/' + self._joint_list[i], timeout=30))
         rospy.Subscriber('/march/gait/schedule/goal', GaitActionGoal, callback=self.gait_selection_callback)
         self._linearize = rospy.get_param('~linearize_gain_scheduling')
 
     def gait_selection_callback(self, data):
-        rospy.logdebug('This is the gait name: %s', data.goal.current_subgait.gait_type)
+        rospy.logdebug('This is the gait type: %s', data.goal.current_subgait.gait_type)
         new_gait_type = data.goal.current_subgait.gait_type
         if new_gait_type is None or new_gait_type == '':
             new_gait_type = 'walk_like'
             rospy.logwarn('The gait has no gait type, default is set to walk_like')
+
         if self._gait_type != new_gait_type or not self.interpolation_done:
             rospy.logdebug('The selected gait: {0} is not the same as the previous gait: {1}'.format(
                 new_gait_type, self._gait_type))
+
+            self.load_current_gains()
             self._gait_type = new_gait_type
             self.interpolation_done = False
             rate = rospy.Rate(10)
@@ -64,6 +64,12 @@ class DynamicPIDReconfigurer:
             rospy.logdebug('Config set to {0}, {1}, {2}'.format(self.current_gains[i][0],
                                                                 self.current_gains[i][1],
                                                                 self.current_gains[i][2]))
+
+    def load_current_gains(self):
+        self.current_gains = []
+        for joint_name in self._joint_list:
+            gains = rospy.get_param('/march/controller/trajectory/gains/' + joint_name)
+            self.current_gains.append([gains['p'], gains['i'], gains['d']])
 
     # Method that pulls the PID values from the gains_per_gait_type.yaml config file
     def look_up_table(self, joint_index):
