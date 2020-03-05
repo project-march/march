93af19c024d86c80a6f943b1b5a5a5d0b02eb879
Roelemans <47626418+Roelemans@users.noreply.github.com>
2020-03-05 12:32:40 +0100

    Add /march/gait/current topic (#479)
    * Add /march/gait/current topic
    
    * Make StepStateMachine inherit from GaitStateMachine
    
    Co-authored-by: JorisWeeda <45165354+JorisWeeda@users.noreply.github.com>

--- march_state_machine/src/march_state_machine/state_machines/step_state_machine.py
+++ march_state_machine/src/march_state_machine/state_machines/step_state_machine.py
@@ -1,10 +1,11 @@
 import smach
 
-from march_state_machine.control_flow import control_flow
 from march_state_machine.states.gait_state import GaitState
 
+from .gait_state_machine import GaitStateMachine
 
-class StepStateMachine(smach.Sequence):
+
+class StepStateMachine(GaitStateMachine):
     """A smach.StateMachine that implements a gait with a sequence of subgaits."""
 
     def __init__(self, gait_name, subgaits=None):
@@ -18,19 +19,13 @@ class StepStateMachine(smach.Sequence):
         :param subgaits: List of subgaits to be performed in the given order.
                          When None, the subgaits will be ['right_open', 'left_close'].
         """
-        super(StepStateMachine, self).__init__(outcomes=['succeeded', 'preempted', 'failed'],
-                                               connector_outcome='succeeded')
+        super(StepStateMachine, self).__init__(gait_name)
 
         if subgaits is None:
             subgaits = ['right_open', 'left_close']
 
         self.open()
         for subgait in subgaits:
-            smach.Sequence.add(subgait, GaitState(gait_name, subgait), transitions={'aborted': 'failed'})
+            smach.Sequence.add_auto(subgait, GaitState(gait_name, subgait), connector_outcomes=['succeeded'],
+                                    transitions={'aborted': 'failed'})
         self.close()
-
-        self.register_termination_cb(self._termination_cb)
-
-    @staticmethod
-    def _termination_cb(_userdata, _terminal_states, _outcome):
-        control_flow.gait_finished()
