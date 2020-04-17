9d33df2ccf9aff6a97126988301887d07fe7a003
RutgerVanBeek <rvbG14-22>
2020-04-17 11:34:37 +0200

    remove pyhton script and set correct duration

--- march_gait_selection/src/march_gait_selection/dynamic_gaits/moveit_subgait.py
+++ march_gait_selection/src/march_gait_selection/dynamic_gaits/moveit_subgait.py
@@ -1,29 +1,23 @@
 import sys
-import copy
-import rospy
-import moveit_commander
-import moveit_msgs.msg
-from geometry_msgs.msg import Pose
-from visualization_msgs.msg import Marker
-from tf.transformations import quaternion_from_euler, quaternion_multiply
-import tf2_ros
 
-from math import pi, asin, sqrt, acos
+import moveit_commander
+import rospy
 
 from march_shared_resources.msg import Subgait
 
 
 class MoveItSubgait(object):
+
     def __init__(self):
         rospy.init_node('moveit_subgait', anonymous=True)
 
         moveit_commander.roscpp_initialize(sys.argv)
 
-        self.robot_ = moveit_commander.RobotCommander()
-        self.scene_ = moveit_commander.PlanningSceneInterface()
+        moveit_commander.RobotCommander()
+        moveit_commander.PlanningSceneInterface()
 
-        self.move_group_right_ = moveit_commander.MoveGroupCommander("right_leg")
-        self.move_group_left_ = moveit_commander.MoveGroupCommander("left_leg")
+        self.move_group_right_ = moveit_commander.MoveGroupCommander('right_leg')
+        self.move_group_left_ = moveit_commander.MoveGroupCommander('left_leg')
 
     def random_subgait(self):
         subgait = Subgait()
@@ -36,11 +30,6 @@ class MoveItSubgait(object):
         subgait.version = 'moveit'
         subgait.description = 'randomly generated subgait by MoveIt'
         subgait.gait_type = 'walk_like'
+        subgait.duration = subgait.trajectory.points[-1].time_from_start
 
         return subgait
-
-
-
-
-
-
