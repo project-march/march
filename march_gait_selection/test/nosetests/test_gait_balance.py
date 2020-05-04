f8f6ec0e1da0815f86382faaeca7dc22d5c608da
jorisweeda <joris.weeda@hotmail.com>
2020-05-04 15:18:03 +0200

    Added the first balance gait test functions.

--- march_gait_selection/test/nosetests/test_gait_balance.py
+++ march_gait_selection/test/nosetests/test_gait_balance.py
@@ -3,6 +3,7 @@
 from copy import deepcopy
 import unittest
 
+from march_gait_selection.dynamic_gaits.balance_gait import BalanceGait
 from march_gait_selection.gait_selection import GaitSelection
 
 PKG = 'march_gait_selection'
@@ -17,3 +18,17 @@ class TestTransitionTrajectory(unittest.TestCase):
 
     def setUp(self):
         self.gait_selection = deepcopy(self._gait_selection)
+
+    # __init__ tests
+    def test_get_balance_walk_gait(self):
+        is_balanced_walk = True if self.gait_selection['gait_balanced_walk'] is not None else False
+        self.assertTrue(is_balanced_walk)
+
+    def test_get_balance_walk_gait_type(self):
+        balanced_walk = self.gait_selection['gait_balanced_walk']
+        self.assertIsInstance(balanced_walk, BalanceGait)
+
+    def test_initialised_move_groups(self):
+        balanced_walk = self.gait_selection['gait_balanced_walk']
+        has_move_groups = True if balanced_walk._move_groups is not None else False
+        self.assertTrue(has_move_groups)
