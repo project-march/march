29c593f7bf0cb4277113d7a5f6eef4423688d4df
Deakensbubbles <36988064+Deakensbubbles@users.noreply.github.com>
2020-05-05 12:08:43 +0200

    Update march_gait_selection/src/march_gait_selection/gait_selection.py
    Co-authored-by: Roelemans <47626418+Roelemans@users.noreply.github.com>

--- march_gait_selection/src/march_gait_selection/gait_selection.py
+++ march_gait_selection/src/march_gait_selection/gait_selection.py
@@ -71,7 +71,7 @@ class GaitSelection(object):
             loaded_gait = Gait.from_file(gait, self.gait_directory, self.robot, self._gait_version_map)
             self.loaded_gaits.append(loaded_gait)
 
-        self.balance_gait.default_walk = next((gait for gait in self.loaded_gaits if gait.gait_name == 'walk'), None)
+        self.balance_gait.default_walk = self['walk']
 
     def scan_directory(self):
         """Scan the gait_directory recursively and create a dictionary of all subgait files.
