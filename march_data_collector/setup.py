d287293e26a0fdcff91d51294f93fa2b5f6b4339
Olav de Haas <o.n.dehaas@student.tudelft.nl>
2020-06-01 13:39:22 +0200

    Fix setup.py again

--- march_data_collector/setup.py
+++ march_data_collector/setup.py
@@ -1,11 +1,10 @@
 # ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
+from catkin_pkg.python_setup import generate_distutils_setup
 from setuptools import setup
 
-from catkin_pkg.python_setup import generate_distutils_setup
-
-# fetch values from package.xml
 setup_args = generate_distutils_setup(
     packages=['march_data_collector'],
+    package_dir={'': 'src'},
     scripts=['scripts/march_data_collector'],
 )
 
--- march_shared_classes/setup.py
+++ march_shared_classes/setup.py
@@ -1,10 +1,10 @@
 # ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
-from setuptools import setup
-
 from catkin_pkg.python_setup import generate_distutils_setup
+from setuptools import setup
 
 setup_args = generate_distutils_setup(
     packages=['march_shared_classes', 'march_shared_classes.gait', 'march_shared_classes.exceptions'],
+    package_dir={'': 'src'},
 )
 
 setup(**setup_args)
