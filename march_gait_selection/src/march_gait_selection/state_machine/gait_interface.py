bbffc77f55ff8bb7bf98adb9f12a0b091cde8555
Olav de Haas <23523462+Olavhaasie@users.noreply.github.com>
2020-07-14 16:04:38 +0200

    Add gait interface

---
+++ march_gait_selection/src/march_gait_selection/state_machine/gait_interface.py
@@ -0,0 +1,33 @@
+class GaitInterface(object):
+    def start(self, starting_position):
+        """Called when the gait has been selected for execution.
+
+        The gait should start at the given starting position.
+        :param starting_position: starting positions of all joints
+        """
+        pass
+
+    def get_final_position(self):
+        """Returns the position of all the joints after the gait has ended."""
+        pass
+
+    def update(self, elapsed_time):
+        """Called in a loop with the elapsed time since the last update.
+
+        :param float elapsed_time: Elapsed time in seconds since the last update
+        :returns A pair of a trajectory and a flag. The trajectory that will be
+                 set as the new goal for the controller, can be None. The flag
+                 indicates whether the gait has finished.
+        """
+        return None, False
+
+    def stop(self):
+        """Called when the gait has been instructed to stop.
+
+        :returns True when the stop action has been accepted, False otherwise.
+        """
+        return False
+
+    def end(self):
+        """Called when the gait has finished."""
+        pass
