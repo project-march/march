3990094968e392f9f9f4cef43be4313a49920dd4
roel <roel@vossenopreis.nl>
2020-07-14 10:24:29 +0200

    Fix class constants and default dt to 0.01

--- march_gait_selection/src/march_gait_selection/dynamic_gaits/inverted_pendulum.py
+++ march_gait_selection/src/march_gait_selection/dynamic_gaits/inverted_pendulum.py
@@ -2,31 +2,32 @@ import math
 
 
 class InvertedPendulum(object):
-    g = 9.81
+    G = 9.81
+    EPSILON = 0.000001
 
     @classmethod
-    def numeric_solve_to_t(cls, x, y, z, vx, vy, t, dt=0.0001):
+    def numeric_solve_to_t(cls, x, y, z, vx, vy, t, dt=0.01):
         time = 0
         while time < t:
             x, y, z, vx, vy = cls.step_numeric_solve(x, y, z, vx, vy, dt)
             time += dt
-        if abs(z) < 0.000001:
+        if abs(z) < EPSILON:
             vz = 0
         else:
             vz = - (x * vx + y * vy) / z
         return {'x': x, 'y': y, 'z': z, 'vx': vx, 'vy': vy, 'vz': vz}
 
     @classmethod
-    def step_numeric_solve(cls, x0, y0, z0, vx0, vy0, dt=0.0001):
+    def step_numeric_solve(cls, x0, y0, z0, vx0, vy0, dt=0.01):
         r = math.sqrt(x0**2 + y0**2 + z0**2)
-        if abs(z0) < 0.000001:
+        if abs(z0) < EPSILON:
             vz0 = 0
         else:
             vz0 = - (x0 * vx0 + y0 * vy0) / z0
         v = math.sqrt(vx0**2 + vy0**2 + vz0**2)
 
-        ax = x0 * (cls.g * z0 - v)
-        ay = y0 * (cls.g * z0 - v)
+        ax = x0 * (cls.G * z0 - v)
+        ay = y0 * (cls.G * z0 - v)
 
         vx1 = vx0 + dt * ax
         vy1 = vy0 + dt * ay
