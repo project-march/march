fc761cac8093f7649c03f8921172f542cb5a8bf8
roel <roel@vossenopreis.nl>
2020-08-02 15:01:44 +0200

    Fix capture point calculation

--- march_data_collector/src/march_data_collector/cp_calculator.py
+++ march_data_collector/src/march_data_collector/cp_calculator.py
@@ -77,27 +77,32 @@ class CPCalculator(object):
             the amount of seconds away from the current time the capture point should be calculated
         """
         rospy.logdebug('time diff: {td}'.format(td=str(self._delta_t)))
-        if self._delta_t == 0:
-            rospy.loginfo('Calculating future center of mass for capture point pose.')
-            new_center_of_mass = InvertedPendulum.numeric_solve_to_t(self.x, self.y, self.z, self.vx, self.vy, duration)
 
-            try:
-                world_transform = self._tf_buffer.lookup_transform('world', self._foot_link, rospy.Time())
+        try:
+            world_transform = self._tf_buffer.lookup_transform('world', self._foot_link, rospy.Time())
+
+            if self._delta_t != 0:
+                rospy.loginfo('Calculating future center of mass for capture point pose.')
+                new_center_of_mass = InvertedPendulum.numeric_solve_to_t(self.x - world_transform.transform.translation.x,
+                                                                         self.y - world_transform.transform.translation.y,
+                                                                         self.z - world_transform.transform.translation.z,
+                                                                         self.vx, self.vy, duration)
+
                 if new_center_of_mass.z <= 0:
                     rospy.logdebug_throttle(1, 'Cannot calculate capture point; center of mass < 0')
 
                 capture_point_multiplier = sqrt(new_center_of_mass.z / self._gravity_constant)
 
-                x_cp = world_transform.transform.translation.x + new_center_of_mass.vx * capture_point_multiplier
-                y_cp = world_transform.transform.translation.y + new_center_of_mass.vy * capture_point_multiplier
+                x_cp = new_center_of_mass.x + new_center_of_mass.vx * capture_point_multiplier
+                y_cp = new_center_of_mass.y + new_center_of_mass.vy * capture_point_multiplier
 
                 self._capture_point_marker.header.stamp = rospy.get_rostime()
-                self._capture_point_marker.pose.position.x = x_cp
-                self._capture_point_marker.pose.position.y = y_cp
+                self._capture_point_marker.pose.position.x = x_cp + world_transform.transform.translation.x
+                self._capture_point_marker.pose.position.y = y_cp + world_transform.transform.translation.y
                 self._capture_point_marker.pose.position.z = 0
 
-            except tf2_ros.TransformException as e:
-                rospy.logdebug('Error in trying to lookup transform for capture point: {error}'.format(error=e))
+        except tf2_ros.TransformException as e:
+            rospy.logdebug('Error in trying to lookup transform for capture point: {error}'.format(error=e))
 
         return {'time': self._center_of_mass_marker.header.stamp, 'x': self.x, 'y': self.y, 'z': self.z}
 
